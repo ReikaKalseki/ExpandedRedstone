1bbb54facf095510f59f8560181e3d8760703032
Reika <reikasminecraft@gmail.com>
2014-04-10 09:21:28 -0400

    Update

--- Base/ExpandedRedstoneTileEntity.java
+++ Base/ExpandedRedstoneTileEntity.java
@@ -44,7 +44,7 @@ public abstract class ExpandedRedstoneTileEntity extends TileEntityBase {
 	}
 
 	@Override
-	public void animateWithTick(World world, int x, int y, int z) {}
+	protected void animateWithTick(World world, int x, int y, int z) {}
 
 	@Override
 	public final int getTileEntityBlockID() {
