3204edb400c2fb29277a2c0058c4652139c0c644
Reika <reikasminecraft@gmail.com>
2014-06-10 17:29:01 -0400

    Update

--- Base/ExpandedRedstoneTileEntity.java
+++ Base/ExpandedRedstoneTileEntity.java
@@ -239,4 +239,9 @@ public abstract class ExpandedRedstoneTileEntity extends TileEntityBase {
 	public final int getRedstoneOverride() {
 		return 0;
 	}
+
+	@Override
+	public final boolean hasModel() {
+		return false;
+	}
 }
